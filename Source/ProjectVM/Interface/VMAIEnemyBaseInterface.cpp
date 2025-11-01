d1d312143c2d1e136dfc71d91403325a63776a8f
soloplay <somnium1108@gmail.com>
2025-11-01 15:21:42 +0900

    Feat: Enemy가 사용할 인터페이스 틀 구현

---
+++ Source/ProjectVM/Interface/VMAIEnemyBaseInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interface/VMAIEnemyBaseInterface.h"
+
+// Add default functionality here for any IVMAIEnemyBaseInterface functions that are not pure virtual.
---
+++ Source/ProjectVM/Interface/VMAIEnemyBaseInterface.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "VMAIEnemyBaseInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UVMAIEnemyBaseInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class PROJECTVM_API IVMAIEnemyBaseInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+
+#pragma region 인터페이스_함수
+public:
+	// Enemy가 필수적으로 구현해야할 함수를 선언.
+	virtual float GetAIPatrolRadius() = 0;
+	virtual float GetAIDetectRange() = 0;
+	virtual float GetAIAttackRange() = 0;
+	virtual float GetAIAITurnSpeed() = 0;
+#pragma endregion
+};
