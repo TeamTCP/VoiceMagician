bb70f61aa9c1157c6a18b3bd1f3c5cc3f6e1ff53
soloplay <somnium1108@gmail.com>
2025-11-20 04:22:22 +0900

    Feat: 보스 벽 생성->BP로 짜기 위해 Function 생성용

--- Source/ProjectVM/Environment/BossWall.h
+++ Source/ProjectVM/Environment/BossWall.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -23,4 +23,9 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION(BlueprintImplementableEvent, Category = "BossWall")
+	void StartLoweringWall();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "BossWallUp")
+	void StartUpperingWall();
 };
